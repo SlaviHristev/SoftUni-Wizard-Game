3f133b8796616f1b0c93e980c7c831ba8ca65085
SlaviHristev <Slavihristev97@gmail.com>
2023-09-27 14:41:46 +0300

    Added movement logic for the wizard

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,63 @@ const gameOver = document.querySelector('.game-over');
 const gameScore = document.querySelector('.score');
 
 
-gameStart.addEventListener('click', onGameStart)
-  
+let keys = {};
 
-function onGameStart(){
+let game = {
+    speed: 2,
+    movingMultip: 4,
+};
+
+let player = {
+    x: 100,
+    y: 100,
+};
+
+gameStart.addEventListener('click', onGameStart);
+
+function onGameStart() {
     gameStart.classList.add('hide');
+    const wizard = document.createElement('div');
+    wizard.classList.add('wizard');
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
+    gameArea.appendChild(wizard);
+    
+
+    
+    window.requestAnimationFrame(gameAction);
 }
 
-const wizard = document.createElement('div');
-wizard.classList.add('wizard');
-wizard.style.top = '200px';
-wizard.style.left = '200px';
-gameArea.appendChild(wizard)
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup',handleKeyUp);
+
+function handleKeyDown(event) {
+    keys[event.code] = true;
+    
+}
+
+function handleKeyUp(event) {
+    keys[event.code] = false;
+    
+}
+
+function gameAction() {
+    const wizard = document.querySelector('.wizard');
+    if (keys['KeyW']) {
+        player.y -= game.speed * game.movingMultip;
+    } 
+    if (keys['KeyS']) {
+        player.y += game.speed * game.movingMultip;
+    }
+
+    if (keys['KeyA']) {
+        player.x -= game.speed * game.movingMultip;
+    }
+    if (keys['KeyD']) {
+        player.x += game.speed * game.movingMultip;
+    }
+
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
+    window.requestAnimationFrame(gameAction);
+}
